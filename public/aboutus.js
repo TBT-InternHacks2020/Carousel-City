bfbb2856208a5c505f8d167bdd7a3581fab35749
Dara12438 <Dara12438@users.noreply.github.com>
2020-07-11 04:28:05 -0400

    about us page in progress

--- public/aboutus.js
+++ public/aboutus.js
@@ -1,7 +1,11 @@
 function pic (name) {
-    var img;
+    var img = document.createElement("img");
+    img.scr = `../images/${name}.png`;
+    img.width = 500;
+    img.height - 500;
+    // if (name == "Dara") {
 
-    if (name == "Dara") {
+    // }
 
-    }
+    document.body.appendChild(img);
 }
