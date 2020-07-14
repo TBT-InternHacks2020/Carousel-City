132ac1d28cc6c36acb03fcb7a9b3e4c9ab96aa4c
HannahGuo <hguo2102@gmail.com>
2020-07-14 03:24:11 -0400

    Updating storefront

--- database.js
+++ database.js
@@ -19,6 +19,9 @@ function readData() {
             database.ref(i).on("value", function (snapshot) {
                 var snap = snapshot.val();
                 rowLB += generateBusinessDiv(snap["Name"], snap["Address"], snap["Type"]);
+                if (i == 0) genereateBusinessStorefront(snap["Name"], "active");
+                else genereateBusinessStorefront(snap["Name"], "");
+
                 rowCounter++;
 
                 if (rowCounter == 3) {
@@ -31,6 +34,24 @@ function readData() {
             });
         }
     });
+}
+
+function genereateBusinessStorefront(name, isActive) {
+    var titleClass = "titleFont";
+    if (name.length > 25) {
+        titleClass = "smallTitleFont";
+    }
+
+    $("#insertStores").append(`<div class="carousel-item ${isActive}">
+    <img src="../images/Storefront.png" class="storefront">
+	<svg viewBox="0 0 1000 500" preserveAspectRatio="xMinYMin meet" class="svg-content">
+    <text x="50%" y="32%" dominant-baseline="middle" text-anchor="middle" class="${titleClass}"">${name}</text>
+    <rect width="522" height="276" x="242" y="168" style="fill:rgb(0,0,255);"/>
+    <rect width="378" height="110" rx="60" ry="68" x="314" y="82" style="fill:rgb(44, 52, 153);" /> 
+    </svg>
+  </div>`);
+    console.log(screen.height / 2);
+    console.log(screen.width / 2)
 }
 
 function generateBusinessDiv(name, address, type) {
